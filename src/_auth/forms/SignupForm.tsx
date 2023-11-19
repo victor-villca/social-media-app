6e88fa70a666669647b1fa0e3d91317544ade522
victor-villca <villcavictorleon@gmail.com>
2023-11-18 21:02:59 -0400

    appwrite auth create user account implementation

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -9,6 +9,7 @@ import { SignUpValidation } from "@/lib/validation"
 import {z} from 'zod'
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
 
 const SignupForm = () => {
   const isLoading =  false
@@ -22,8 +23,9 @@ const SignupForm = () => {
     },
   })
  
-  function onSubmit(values: z.infer<typeof SignUpValidation>) {
-    console.log(values)  
+  async function onSubmit(values: z.infer<typeof SignUpValidation>) {
+    const newUser = await createUserAccount(values)
+    console.log(newUser)
   }
   return (
     <Form {...form}>
